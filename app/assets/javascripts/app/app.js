eeeffb283d85e220e1a06dd26e7f0672aff44ee2
Ken Decanio <github.com@justthisguy.org>
2013-06-22 05:11:38 -0700

    the layout is now part of the OXO game

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -3,20 +3,20 @@ var OXO = {
   Collections: {},
   Views: {},
   Presenters: {},
-  Router: {}
+  Router: {},
+  turn: 'x',
+  layout: []
 };
 
 OXO.initialize = function() {
 
-  this.set({turn: 'x'});
-
-	var board = new OXO.Models.Board(the_layout);
+	var board = new OXO.Models.Board({layout: OXO.layout});
 	OXO.Views.board = new OXO.Views.Board({model:board, el: '#oxo-board'});
 	OXO.Views.board.render();
 
 //	Backbone.history.start();
 };
 
-var the_layout = { layout:  [ ["upper-left", "upper-middle", "upper-right"],
-                              ["middle-left", "middle-middle", "middle-right"],
-                              ["lower-left", "lower-middle", "lower-right"] ] }
+OXO.layout = [["upper-left", "upper-middle", "upper-right"],
+              ["middle-left", "middle-middle", "middle-right"],
+              ["lower-left", "lower-middle", "lower-right"] ]
